b71ec4ead21696889e21922d11c32164c55fec87
mizznoff <mizznoff+dev@gmail.com>
2025-10-30 19:38:32 +0900

    トップ画面の実装

---
+++ Letters/ViewModel/IndexViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  IndexViewModel.swift
+//  Letters
+//
+//  Created by mizznoff on 2025/10/29.
+//
+
+import Combine
+import Foundation
+import PhotosUI
+import UIKit
+import SwiftUI
+
+public final class IndexViewModel: NSObject, ObservableObject {
+    @Published public var uiImage: UIImage?
+    @Published public var pickerItem: PhotosPickerItem? {
+        didSet {
+            guard let pickerItem else {
+                uiImage = nil
+                return
+            }
+            pickerItem.loadTransferable(type: Data.self) { result in
+                Task { @MainActor in
+                    guard pickerItem == self.pickerItem else {
+                        return
+                    }
+                    switch result {
+                    case let .success(data):
+                        if let data, let uiImage = UIImage(data: data) {
+                            self.uiImage = uiImage
+                        }
+                    default:
+                        break
+                    }
+                }
+            }
+        }
+    }
+}
