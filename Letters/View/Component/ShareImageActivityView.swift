cd7ded6cab66ef7dd240d5751c9f4b55bfcf7891
mizznoff <mizznoff+dev@gmail.com>
2025-11-08 11:40:50 +0900

    共有機能

---
+++ Letters/View/Component/ShareImageActivityView.swift
@@ -0,0 +1,26 @@
+//
+//  ShareImageActivityView.swift
+//  Letters
+//
+//  Created by mizznoff on 2025/11/08.
+//
+
+import Foundation
+import SwiftUI
+import UIKit
+
+public struct ShareImageActivityView: UIViewControllerRepresentable {
+    private let uiImage: UIImage
+
+    public init(uiImage: UIImage) {
+        self.uiImage = uiImage
+    }
+
+    public func makeUIViewController(context _: Context) -> UIActivityViewController {
+        UIActivityViewController(activityItems: [uiImage], applicationActivities: nil)
+    }
+
+    public func updateUIViewController(_: UIActivityViewController, context _: Context) {
+        // NOP
+    }
+}
